fae38f2bc888d9a83d814cb71f2a5caf12cee9d2
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-08-17 15:51:30 +0400

    Cleared scripts.js to a fresh start of new project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,1 @@
-let myName='Siavash';
-let myAge= 37;
-let favoriteFood= 'Fried Fish'
-console.log(myName, " is ", (myAge+1));
-alert('Hello World! '+ myName + ' is ' + (myAge+1) + ' Years Old!');
-document.write(myName +"'s" +' favorite food is ' + favoriteFood);
+// Pokémon project
