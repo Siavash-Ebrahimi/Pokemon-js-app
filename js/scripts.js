341f9cddc2814ce16c79e23103c5a5dda8a0b857
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-08-17 16:23:29 +0400

    Added array include 4 Pokémon objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 // Pokémon project
+// Creating Pokémon Array
+let pokemonList = [
+  {name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison'], weaknesses: ['Fire', 'Psychic', 'Flying', 'Ice']},
+  {name: 'Blastoise', height: 5.03, types: 'water', weaknesses: ['Grass', 'Electric']},
+  {name: 'Charizard', height: 5.07, types: ['fire', 'flying'], weaknesses: ['Water', 'Electric', 'Rock']},
+  {name: 'Wigglytuff', height: 3.03, types: ['normal', 'fairy'], weaknesses: ['Steel', 'Poison']},
+];
+console.log(pokemonList);
