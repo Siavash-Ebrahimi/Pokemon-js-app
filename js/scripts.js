7036a0f1e73a03646c729162a6987c85a7a5f7e8
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-08-25 16:10:21 +0400

    Replaced code with Internal forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,8 @@ let pokemonList = [
   {name: 'Charizard', height: 5.07, types: ['fire', 'flying'], weaknesses: ['Water', 'Electric', 'Rock']},
   {name: 'Wigglytuff', height: 3.03, types: ['normal', 'fairy'], weaknesses: ['Steel', 'Poison']},
 ];
-//Change a name of a pokemon;
-pokemonList[0].name = 'Siavash';
-/* Iterating all items in pokemonList by
-   using for loop based on pokemonList length */
-for (let i = 0; i < pokemonList.length; i++) {
-  /* Use if condition for searching in all heights
-     related to find uper 5.05 during thr for loop then
-     once find it it will return it in a document.write*/
-  if (pokemonList[i].height >= 5.05) {
-    document.write(`<p>Pokémon name: ${pokemonList[i].name}, (height: ${pokemonList[i].height}) - Wow, that’s big!</p>`);
-  } else {
-    document.write(`<p>Pokémon name: ${pokemonList[i].name}, (height: ${pokemonList[i].height})</p>`);
-  }
-}
-document.write('<h1 class="java">Siavash Ebrahimi</h1>')
+
+// Print all Pokémon Detailes by Internal using forEach loop:
+pokemonList.forEach(function(i) {
+   document.write(`<p>Pokémon name: ${i.name}, he is ${i.height} tall, with type of: ${i.types}, and the Weakness of: ${i.weaknesses}`);
+});
