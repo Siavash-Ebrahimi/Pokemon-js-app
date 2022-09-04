ce45a293d2896ebf06602985a470e43dcf972c34
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-09-04 09:56:07 +0400

    Used Ajax to fetch data from Pokemon API
    The data will collect from Pokemon API and by using Ajax (Promise, JSON, HTTP) , the selected item will use at client-side.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
 // Pokémon project
-// Creating Pokémon Array
-let pokemonRepository = (function(){
-  let pokemonList = [
-    {name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison'], weaknesses: ['Fire', 'Psychic', 'Flying', 'Ice']},
-    {name: 'Blastoise', height: 5.03, types: ['water'], weaknesses: ['Grass', 'Electric']},
-    {name: 'Charizard', height: 5.07, types: ['fire', 'flying'], weaknesses: ['Water', 'Electric', 'Rock']},
-    {name: 'Wigglytuff', height: 3.03, types: ['normal', 'fairy'], weaknesses: ['Steel', 'Poison']},
-  ];
 
+// This pokemonRepository is an IIFE that wrap all pokemon
+// function like a big box.
+let pokemonRepository = (function(){
+  let pokemonList = [];
+  let urlApi = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  // Push Pkemon into the pokemonList.
   function add(item){
-    pokemonList.push(item);
+    if(typeof item === "object" && "name" in item){
+        pokemonList.push(item);
+    }else {
+      console.log('Pokemon name in not correct');
+    }
   }
 
+  // A simple function to show all pokemonList.
   function allGet(){
     return pokemonList;
   }
 
-  function showDetails (pokemon, button){
-    button.addEventListener('click', function(event){
-      console.log("Pokémon Name:" + pokemon.name + ", " + "Pokémon Height:" + pokemon.height);
-    });
-  }
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+ }
 
   let pokdex = document.querySelector('.pokomen-list');
 
@@ -31,24 +35,58 @@ let pokemonRepository = (function(){
     button.classList.add('pokItem');
     itemList.appendChild(button);
     pokdex.appendChild(itemList);
-    showDetails(pokemon, button);
+    button.addEventListener('click', function(event){
+      showDetails(pokemon);
+    });
+  }
+
+  // fetch all data from API Pokomen and collect only the data
+  // inside the result.
+  function loadList() {
+    return fetch(urlApi).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      item.weight = details.weight;
+    }).catch(function (e) {
+      console.error(e);
+    });
   }
 
   return {
     add: add,
     allGet: allGet,
-    addListItem: addListItem
+    loadList: loadList,
+    loadDetails: loadDetails,
+    addListItem: addListItem,
+    showDetails: showDetails
   }
 
 })()
 
-pokemonRepository.add({name: 'Diandong', height: 5.05, types:'', weaknesses:''});
-
-pokemonRepository.allGet().forEach(function(i){
-  pokemonRepository.addListItem(i);
-})
-
-// // Print all Pokémon Detailes by Internal using forEach loop:
-// pokemonRepository.allGet().forEach(function(i) {
-//    document.write(`<p>Pokémon name: ${i.name}, he is ${i.height} tall, with type of: ${i.types}, and the Weakness of: ${i.weaknesses}`);
-// });
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.allGet().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
